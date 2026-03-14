3579d07d77cbecb78154b56a7d8f28016a044705
minkyu kang <mike3578@naver.com>
2026-03-14 21:17:39 +0900

    update

---
+++ knking_problems/chapter12/palindrome.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+#define LEN 100
+
+bool is_palindrome(char arr[], int count);
+
+int main(void){
+
+    char ch;
+    char *p, arr[LEN];
+    int count = 0;
+    p = arr;
+
+    printf("Enter a message: ");
+
+    while((ch = getchar()) != '\n' && ch != EOF){
+        ch = tolower(ch);
+        if(ch <= 'x' && ch >= 'a'){
+            *p = ch;
+            p++; count++;
+        }
+    }
+
+    if(is_palindrome(arr, count) == true)
+        printf("Palindrome\n");
+    else
+        printf("Not a palindrome\n");
+
+    //system("pause");
+    return 0;
+}
+
+bool is_palindrome(char arr[], int count){
+
+    char *p, *q;
+    q = (arr + count - 1);
+    printf("%c\n", *q);
+    count = (count%2 == 1) ? count/2 - 1 : count/2;
+    
+    for(p = arr; p < arr + count; p++, q--){
+        if(*p != *q){
+            return false;
+        }
+    }
+    return true;
+}
